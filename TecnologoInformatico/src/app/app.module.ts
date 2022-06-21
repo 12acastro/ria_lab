cc66df3205ca4a08cf3ea493ac580d047a3bdbf5
Nahuel Perodmo <nahuperdomo123@gmail.com>
2022-06-21 20:30:20 -0300

    Primeros pasos

--- TecnologoInformatico/src/app/app.module.ts
+++ TecnologoInformatico/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ClarityModule } from "@clr/angular";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
